94b15fcfd91417beb6e8da61e288c3dc0c867c8b
Yap Kaa Sheng <ksyap@ifca.com.my>
2017-11-04 17:16:57 +0800

    Image

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -4,9 +4,9 @@ angular.module('mean.system')
   .controller('IndexController', ['$scope', '$location', 'Global', function ($scope, $location, Global) {
       $scope.global = Global;
 
-      $scope.productImg1 = "img/Gary.jpg";
-      $scope.productImg2 = "https://6.viki.io/image/ffc346eeaae0494d84e1a0943eef2399.jpeg?x=b&a=0x0&s=460x268&e=t&f=t&cb=1";
-      $scope.productImg3 = "http://img.etnews.com/news/article/2016/04/04/article_04104822604409.jpg";
+      $scope.productImg1 = "img/womenshirt.jpg";
+      $scope.productImg2 = "img/shoes.jpg";
+      $scope.productImg3 = "img/shirt.jpg";
 
       $('.carousel').carousel();
       
