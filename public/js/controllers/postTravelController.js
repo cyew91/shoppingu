3e3fcb24129093350bf84e504a1b816a43a05cc0
certong <tongtong0507@live.com>
2017-11-20 23:43:00 +0800

    Post travel date range update

--- public/js/controllers/postTravelController.js
+++ public/js/controllers/postTravelController.js
@@ -4,11 +4,15 @@ angular.module('mean').controller('PostTravelController', ['$scope', 'GetCountry
 
     $scope.countryList = [];
     $scope.fooDate = new Date;
+    var totalFields = 10;
 
     const init = function () {
         GetCountryList.query(function (list) {
             $scope.countryList = list;
         });
+
+        totalFields = $("#input-progress").find("input, select").length;
+        updateInputProgress();
     };
 
     init();
@@ -16,6 +20,41 @@ angular.module('mean').controller('PostTravelController', ['$scope', 'GetCountry
     $('.js-example-basic-single').select2();
 
     $('.input-daterange').datepicker({
+        autoclose: true,
+        keepEmptyValues: true,
         format: 'dd-mm-yyyy'
     });
+
+    $('#datepickerFrom').datepicker().on('changeDate', function (e) {
+        updateInputProgress();
+    });
+
+    $('#datepickerTo').datepicker().on('changeDate', function (e) {
+        updateInputProgress();
+    });
+
+    function updateInputProgress() {
+        var filledFields = 0;
+        $("#input-progress").find("input, select").each(function () {
+            if ($(this).val() != "") {
+                filledFields++;
+            }
+        });
+
+        var percent = Math.ceil(100 * filledFields / totalFields);
+        $("#progress-inputs .progress-bar").attr("aria-valuenow", percent).width(percent + "%").find(".sr-only").html(percent + "% Complete");
+
+        return percent;
+    }
+
+    //Input Progress
+    $("#input-progress").click(function () {
+        updateInputProgress();
+    });
+    $("#input-progress .btn-success").click(function () {
+        var percent = updateInputProgress();
+        if (percent == 100) {
+            alert("Finished inputs successfully!");
+        }
+    })
 }]);
