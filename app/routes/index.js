63a43be17cac0381c2d761c8c2847ecb805e2850
Pong <jingwong25@gmail.com>
2018-01-27 11:03:54 +0800

    Fix missing index controller issue

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,7 @@
 'use strict';
 
-module.exports = function(app) {
-// Home route
-var index = require('../../app/controllers/index');
-app.get('/', index.render);
+module.exports = function (app) {
+    // Home route
+    var index = require('../../app/controllers/index');
+    app.get('/', index.render);
 };
-// app.get('/', function(req, res){
-//     res.send(req.session.passport.user);
-// }, index.render);
-// };
-
