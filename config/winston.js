f9b0c692965f58994d245a19f74fbeebd9080a68
Pong <jingwong25@gmail.com>
2018-08-16 22:17:46 +0800

    Enhance log file feature
    Enhance log file feature

--- config/winston.js
+++ config/winston.js
@@ -4,18 +4,30 @@
  * Created by Junaid Anwar on 5/28/15.
  */
 var winston = require('winston');
-var logger = new (winston.Logger)();
 
-logger.add(winston.transports.Console, {
+const logger = winston.createLogger({
     level: 'verbose',
-    prettyPrint: true,
-    colorize: true,
-    silent: false,
-    timestamp: false
+    format: winston.format.json(),
+    transports: [
+        //
+        // - Write to all logs with level `info` and below to `combined.log` 
+        // - Write all logs error (and below) to `error.log`.
+        //
+        new winston.transports.File({
+            filename: 'error.log',
+            level: 'error',
+            prettyPrint: true,
+            colorize: true
+        }),
+        new winston.transports.File({
+            filename: 'combined.log',
+            level: 'verbose'
+        })
+    ]
 });
 
 logger.stream = {
-    write: function(message, encoding){
+    write: function (message, encoding) {
         logger.info(message);
     }
 };
