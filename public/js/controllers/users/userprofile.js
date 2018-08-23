c2c0740cd624c9a51c9f409e62279de2878be6b4
yap kaa sheng <ykssheng@gmail.com>
2018-08-24 07:41:46 +0800

    account page
    account page

--- public/js/controllers/users/userprofile.js
+++ public/js/controllers/users/userprofile.js
@@ -4,51 +4,51 @@ angular.module('mean.articles')
   .controller('UserProfileController', ['$scope', 'Global', '$stateParams', '$state', 'GetUser', '$rootScope', function($scope, Global, $stateParams, $state, GetUser, $rootScope){
     $scope.global = Global;
     // $scope.profileId = $stateParams.profileId;
-    $scope.profileId = $rootScope.currentUser.ProfileID;
+    // $scope.profileId = $rootScope.currentUser.ProfileID;
 
-    const init = function() {
-      GetUser.get({
-        profileId: $scope.profileId
-      }, function(result) {
-          $scope.profile = result;
-          if ($scope.profile.Gender == 0){
-            $scope.checkFemale = "active";
-            $scope.checkMale = "inactive";
-          }
-          else{
-            $scope.checkMale = "active";
-            $scope.checkFemale = "inactive";
-          }
+    // const init = function() {
+    //   GetUser.get({
+    //     profileId: $scope.profileId
+    //   }, function(result) {
+    //       $scope.profile = result;
+    //       if ($scope.profile.Gender == 0){
+    //         $scope.checkFemale = "active";
+    //         $scope.checkMale = "inactive";
+    //       }
+    //       else{
+    //         $scope.checkMale = "active";
+    //         $scope.checkFemale = "inactive";
+    //       }
 
-      });
-    };
+    //   });
+    // };
     
-    init();
+    // init();
 
-    $scope.accountMenu = [{accId: 'a1',accName: 'Profiles'}, {accId: 'a2',accName: 'Addresses'}];
+    // $scope.accountMenu = [{accId: 'a1',accName: 'Profiles'}, {accId: 'a2',accName: 'Addresses'}];
 
-    $('.input-daterange').datepicker({
-      autoclose: true,
-      keepEmptyValues: true,
-      format: 'dd-mm-yyyy',
-      clearBtn: true
-    });
+    // $('.input-daterange').datepicker({
+    //   autoclose: true,
+    //   keepEmptyValues: true,
+    //   format: 'dd-mm-yyyy',
+    //   clearBtn: true
+    // });
 
-    $('#datepickerFrom').on('changeDate', function() {
-      $scope.productObj.startDate = $('#datepickerFrom').datepicker('getFormattedDate');
-    });
+    // $('#datepickerFrom').on('changeDate', function() {
+    //   $scope.productObj.startDate = $('#datepickerFrom').datepicker('getFormattedDate');
+    // });
 
-    $scope.updateProfile = function() {
-      var profile = $scope.profile;
-      if (!profile.updated) {
-          profile.updated = [];
-      }
-      profile.updated.push(new Date().getTime());
-      //profile.$update(function() {
-        //$state.go('home');
-      profile.$update();
-      //});
-    };
+    // $scope.updateProfile = function() {
+    //   var profile = $scope.profile;
+    //   if (!profile.updated) {
+    //       profile.updated = [];
+    //   }
+    //   profile.updated.push(new Date().getTime());
+    //   //profile.$update(function() {
+    //     //$state.go('home');
+    //   profile.$update();
+    //   //});
+    // };
 
     
 
