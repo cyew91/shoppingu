d3f34c63753fe6ce11dca22ac132380678453a7b
cyew91 <30837352+cyew91@users.noreply.github.com>
2018-10-07 01:51:30 +0800

    remove command

--- app/routes/ProductCatSubCatRoutes.js
+++ app/routes/ProductCatSubCatRoutes.js
@@ -3,11 +3,6 @@
 var productCatSubCat = require('../../app/controllers/ProductCatSubCatControllers');
 
 module.exports = function (app) {
-    // User Profile Routes
-    // app.route('/productcat/:profileId')
-    // .get(productCatSubCat.show)
-    // .put(productCatSubCat.updateProfile, productCatSubCat.updateAddress);
-
     app.route('/productcat')
     .get(productCatSubCat.getProductCat);
 
