0f6255e37e2024427f7cceffe485d78a3b97e35d
cyew91 <30837352+cyew91@users.noreply.github.com>
2018-12-23 18:02:50 +0800

    image slider changes

--- public/js/controllers/productDetailsController.js
+++ public/js/controllers/productDetailsController.js
@@ -8,20 +8,59 @@ angular.module('mean').controller('ProductDetailsController', ['$scope', '$state
   // $scope.productName = "testing"
   // $(".lightgallery").lightGallery();
 
-  $('.owl-carousel').owlCarousel({
-    items: 5,
-    loop: true,
-    margin: 10,
-    merge: true,
-    responsive: {
-      678: {
-        mergeFit: true
-      },
-      1000: {
-        mergeFit: false
-      }
+//   $('.owl-carousel').owlCarousel({
+//     items: 5,
+//     loop: true,
+//     margin: 10,
+//     merge: true,
+//     responsive: {
+//       678: {
+//         mergeFit: true
+//       },
+//       1000: {
+//         mergeFit: false
+//       }
+//     }
+//   });
+
+// Product Gallery
+	//------------------------------------------------------------------------------
+	// var $productCarousel = $('.product-carousel');
+	// if($productCarousel.length) {
+
+		//Carousel init
+		// $productCarousel.owlCarousel({
+		// 	items: 1,
+		// 	loop: false,
+		// 	dots: false,
+		// 	URLhashListener: true,
+		// 	startPosition: 'URLHash',
+		// 	onTranslate: activeHash
+		// });
+
+	// 	function activeHash(e) {
+	// 		var i = e.item.index;
+	// 		var $activeHash = $('.owl-item').eq(i).find('[data-hash]').attr('data-hash');
+	// 		$('.product-thumbnails li').removeClass('active');
+	// 		$('[href="#' + $activeHash + '"]').parent().addClass('active');
+	// 		$('.gallery-wrapper .gallery-item').removeClass('active');
+	// 		$('[data-hash="' + $activeHash + '"]').parent().addClass('active');
+			
+	// 	}
+    // }
+    
+    $scope.thumbs = function activeHash(e) {
+        var i = e;
+        var $activeHash = $('.owl-item').eq(i).find('[data-hash]').attr('data-hash');
+        $('.product-thumbnails li').removeClass('active');
+        $('[href="#' + $activeHash + '"]').parent().addClass('active');
+        $('.gallery-wrapper .gallery-item').removeClass('active');
+        $('[data-hash="' + $activeHash + '"]').parent().addClass('active');
+        
     }
-  });
+           
 
 }]);
+
+
 
