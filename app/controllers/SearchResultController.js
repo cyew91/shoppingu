7ff52c2f783f233aa1ba4dd930714ee1a9018d66
cyew91 <30837352+cyew91@users.noreply.github.com>
2019-01-13 14:21:32 +0800

    chat_function_changes

--- app/controllers/SearchResultController.js
+++ app/controllers/SearchResultController.js
@@ -13,16 +13,29 @@ exports.show = function (req, res) {
  * Use in search result page
  */
 exports.getProductDetailByProdName = function (req, res, next, ProductName) {
-    db.post_travel_product.findAll({
-            where: {
-                product_name: {
-                    $like: '%' + ProductName + '%'
-                }
-            },
+    db.post_travel.findAll({
             include: [
-                // {model: db.t_product, where: {PostType: 0}}
                 {
-                    model: db.post_travel_product_document
+                    model: db.post_travel_product,
+                    where: {
+                        product_name: {
+                            $like: '%' + ProductName + '%'
+                        }
+                    },
+                    include: [
+                        {
+                            model: db.post_travel_product_document
+                        },
+                        {
+                            model: db.product_category
+                        },
+                        {
+                            model: db.product_sub_category
+                        }
+                    ]
+                },
+                {
+                    model: db.profile
                 }
             ]
         })
