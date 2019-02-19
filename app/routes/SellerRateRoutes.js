d5d1a31c17f87103847f75301795faa11d25d262
cyew91 <30837352+cyew91@users.noreply.github.com>
2019-02-19 23:57:29 +0800

    seller rate routes update

--- app/routes/SellerRateRoutes.js
+++ app/routes/SellerRateRoutes.js
@@ -1,66 +1,19 @@
 'use strict';
 
-const postTravelProduct = require('../controllers/PostTravelProductController');
-
-var filePath = 'public/uploads/';
-var multer = require('multer');
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, filePath);
-    },
-    filename: function (req, file, cb) {
-      cb(null, req.body.myFileName);
-    }
-});
-var upload = multer({ storage: storage }).any();
-
-const fs = require('fs');
+/**
+ * Module dependencies.
+ */
+var sellerRate = require('../../app/controllers/SellerRateControllers');
 
 module.exports = function (app) {
+    // User Profile Routes
+    app.route('/sellerrate/:id')
+    .get(sellerRate.show)
+    .put(sellerRate.updateSellerRate);
 
-    app.param('postTravelProductId', postTravelProduct.getPostTravelProductById);
+    app.route('/sellerrate')
+    .post(sellerRate.createSellerRate)
+    .get(sellerRate.all);
 
-    // Get posted product in Account - My Trips
-    app.param('postTravelId', postTravelProduct.getPostTravelProductByTravelId);
-    app.route('/posttravelproduct/:postTravelId')
-    .get(postTravelProduct.show)
-
-    app.route('/posttravelproduct/:postTravelProductId')
-        .get(postTravelProduct.show)
-        .put(postTravelProduct.updateProduct);
-
-    // Get features product in home page
-    app.route('/posttravelproduct')
-        .get(postTravelProduct.all);
-        //.post(postTravelProduct.createProduct, postTravelProduct.createProductDocument);
-
-    app.route('/posttravelproduct/productcategory')
-        .get(postTravelProduct.getPostTravelProductByProductCategoryId);
-
-    app.route('/posttravelproduct/profile')
-        .get(postTravelProduct.getPostTravelByProfileId);
-
-
-    //Dropzone: Upload product image
-    // app.post('/uploadProductImage', product.uploadProductImage);
-    app.post('/uploadProductImage', function (req, res) {
-        upload(req, res, function (err) {
-          if (err) {
-            return res.json({ success: false , message: 'Great'});
-          }
-          return res.json({ success: true, message: req.files});
-          // Everything OK
-        });
-    });
-    
-    app.post('/deleteProductImage', function (req, res) {
-        //filePath = filePath + fileNameDateNow;
-        fs.unlink(filePath + req.body.myName, function (err) {
-            if (err) {
-                return res.json({ success: false , message: 'Great'});
-            }
-            return res.json({ success: true, message: req.body.myName});
-            // Everything OK
-        });
-    });
+    app.param('id', sellerRate.getSellerRateById);
 };
